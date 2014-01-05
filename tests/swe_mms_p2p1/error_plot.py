2a377f19cd693a67e38b62b01fe68be9d6c435e2
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-01-05 22:04:06 +0000

    More renaming. Get the Makefile to clean up .pvd files. Also updated the error plotting script.

--- tests/swe_mms_p2p1/error_plot.py
+++ tests/swe_mms_p2p1/error_plot.py
@@ -1,9 +1,9 @@
 import pylab
 
-dx = [0.1, 0.05, 0.025]
-h_error = [1.176096, 0.405122, 0.191630] # Error in L2 norm
-first_order = [(1.0/2.0)**i for i in range(0, len(h_error))]
-second_order = [(1.0/4.0)**i for i in range(0, len(h_error))]
+dx = [0.2, 0.1, 0.05]
+h_error = [0.00348261433835, 0.00113851249434, 0.000398910404953] # Error in L2 norm
+first_order = [1.0e-2*(1.0/2.0)**i for i in range(0, len(h_error))]
+second_order = [1.0e-2*(1.0/4.0)**i for i in range(0, len(h_error))]
 
 pylab.loglog(dx, h_error, label="Free surface height")
 pylab.loglog(dx, first_order, label="First-order convergence")
@@ -11,5 +11,5 @@ pylab.loglog(dx, second_order, label="Second-order convergence")
 pylab.legend()
 pylab.xlabel("Characteristic element length")
 pylab.ylabel("Error in L2 norm")
-pylab.axis([0.01, 1, 0.01, 10])
-pylab.savefig("mms.png")
+pylab.axis([0.01, 1.0, 0.0001, 0.1])
+pylab.savefig("swe_mms_p2p1_error.png")
