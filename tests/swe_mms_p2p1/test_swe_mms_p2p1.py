991fb14eb5fbfdbd0645a50572d16918fe6e1b94
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-01-10 18:32:45 +0000

    Refactor test_swe_mms_p2p1 to work from any directory

--- tests/swe_mms_p2p1/test_swe_mms_p2p1.py
+++ tests/swe_mms_p2p1/test_swe_mms_p2p1.py
@@ -1,32 +1,37 @@
+import os
 import pytest
 import numpy
 from firedrake import *
 
 import shallow_water
 
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+@pytest.fixture(scope='session')
+def input():
+   os.system("make -C " + cwd)
+
 def swe_mms_p2p1():
    configs = ["MMS_A", "MMS_B", "MMS_C"]
    norms = []
-   
+
    for c in configs:
-      sw = shallow_water.ShallowWater(path = c + ".swml")
+      sw = shallow_water.ShallowWater(path=os.path.join(cwd, c + ".swml"))
       sw.run()
       h_old = sw.h_old
-      
+
       f = Function(sw.function_spaces["FreeSurfaceFunctionSpace"])
       f.interpolate(Expression("sin(x[0])*sin(x[1])"))
-      
+
       norms.append(sqrt(assemble(dot(h_old - f, h_old - f) * dx)))
-   
+
    return norms
-      
-def test_swe_mms_p2p1():
+
+def test_swe_mms_p2p1(input):
    norms = numpy.array(swe_mms_p2p1())
    convergence_order = numpy.log2(norms[:-1] / norms[1:])
    print "Convergence order:", convergence_order
    assert (numpy.array(convergence_order) > 1.3).all()
-      
+
 if __name__ == '__main__':
-   import os
-   os.system("make")
    pytest.main(os.path.abspath(__file__))
