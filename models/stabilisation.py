3c55b04288022ef4da4e53b049db1a710deabec4
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-02-19 14:53:11 +0000

    Updated SU stabilisation term to use the "critical rule".

--- models/stabilisation.py
+++ models/stabilisation.py
@@ -9,22 +9,44 @@ def streamline_upwind(mesh, dimension, w, u, u_k):
    h = CellSize(mesh)
    scaling_factor = 0.5
    
-   magnitude = fields_calculations.magnitude_vector(u_k)
+   magnitude = fields_calculations.magnitude_vector(mesh, u_k)
 
    u_nodes = magnitude.vector()
    for i in range(0, len(u_nodes)):
       if(u_nodes[i] < 1.0e-16):
          u_nodes[i] = 1.0e-16
 
-   #k = 10.0 # Viscosity
-   #grid_pe = (magnitude*h)/(2.0*k)
-   #k_bar = ( (1.0/tanh(grid_pe)) - (1.0/grid_pe) ) * k * grid_pe
+   k = 10.0 # Viscosity
 
-   k_bar = scaling_factor*h*magnitude
+   grid_pe = fields_calculations.grid_peclet_number(mesh, u_k, k, magnitude)   
+   grid_pe_nodes = grid_pe.vector()
+   for i in range(0, len(grid_pe_nodes)):
+      if(grid_pe_nodes[i] < 1.0e-16):
+         grid_pe_nodes[i] = 1.0e-16
+   for i in range(0, len(grid_pe_nodes)):
+      if(grid_pe_nodes[i] < -1.0):
+         grid_pe_nodes[i] = -1.0 - 1.0/grid_pe_nodes[i]
+      elif(grid_pe_nodes[i] >= -1.0 and grid_pe_nodes[i] <= 1.0):
+         grid_pe_nodes[i] = 0.0
+      else:
+         grid_pe_nodes[i] = 1.0 + 1.0/grid_pe_nodes[i]
+
+   #grid_pe = (magnitude*h)/(2.0*k)
+
+   #k_bar = grid_pe*h
+
+   #grid_pe = (magnitude*h)/(2.0*k)
+#   k_bar = ( (1.0/tanh(grid_pe)) - (1.0/grid_pe) ) * h
+   k_bar = grid_pe*h*magnitude
 
    F = 0
-   for dim in range(dimension):
-      F += (k_bar/(magnitude**2))*inner(u_k[dim], u_k[dim])*inner(grad(u[dim]), grad(w[dim]))*dx
+   for dim_i in range(dimension):
+      for dim_j in range(dimension):
+         F += 0.5*(k_bar/(magnitude**2))*inner(dot(grad(w[dim_i])[dim_j], u[dim_j]), dot(grad(u[dim_i])[dim_j], u[dim_j]))*dx
+
+   #F = 0
+   #for dim in range(dimension):
+   #   F += 0.5*(k_bar/(magnitude**2))*inner(u[dim], u[dim])*inner(grad(u[dim]), grad(w[dim]))*dx
 
    return F
 
