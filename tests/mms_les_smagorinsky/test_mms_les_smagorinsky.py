1d9595c0d51da8044c4c457e585e6b5523f48886
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-03-20 18:05:24 +0000

    Corrected the exact solution.

--- tests/mms_les_smagorinsky/test_mms_les_smagorinsky.py
+++ tests/mms_les_smagorinsky/test_mms_les_smagorinsky.py
@@ -9,11 +9,12 @@ def mms_les_smagorinsky():
    errors = []
    for n in [4, 8, 16, 32]:
       mesh = UnitSquareMesh(n, n)
-      smagorinsky_coefficient = 1.0*(1.0/n) # Remember to halve the Smagorinsky coefficient each time to compensate for the smaller filter width
+      smagorinsky_coefficient = 1.0
+      filter_width = sqrt((1.0/n)**2 + (1.0/n)**2) # FIXME: Use CellSize instead.
       function_space = FunctionSpace(mesh, "CG", 1)
       u = [Function(function_space).interpolate(Expression('sin(x[0])')), Function(function_space).interpolate(Expression('cos(x[0])'))]
       density = Function(function_space).interpolate(Expression("1.0"))
-      exact_solution = Function(function_space).interpolate(Expression('%f * sqrt(2.0*cos(x[0])*cos(x[0]) + 0.5*sin(x[0])*sin(x[0]) + 0.5*sin(x[0])*sin(x[0]))' % smagorinsky_coefficient))
+      exact_solution = Function(function_space).interpolate(Expression('pow(%f*%f, 2) * sqrt(2.0*cos(x[0])*cos(x[0]) + 0.5*sin(x[0])*sin(x[0]) + 0.5*sin(x[0])*sin(x[0]))' % (smagorinsky_coefficient, filter_width)))
 
       les = LES(mesh, function_space)
       visc = les.eddy_viscosity(u, density, smagorinsky_coefficient, 1.0/n)
@@ -26,7 +27,7 @@ def test_mms_les_smagorinsky():
    errors = numpy.array(mms_les_smagorinsky())
    convergence_order = numpy.log2(errors[:-1] / errors[1:])
    print "Eddy viscosity convergence order:", convergence_order
-   assert (numpy.array(convergence_order) > 1.6).all()
+   assert (numpy.array(convergence_order) > 1.9).all()
    
 if __name__ == '__main__':
    pytest.main(os.path.abspath(__file__))
