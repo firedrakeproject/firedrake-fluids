1926a644173fbaeff6c74554262f87e27a0237bb
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-03-20 13:51:55 +0000

    Added a test case for the new grid Reynolds number diagnostic.

---
+++ tests/diagnostics_grid_reynolds_number/test_diagnostics_grid_reynolds_number.py
@@ -0,0 +1,33 @@
+import os
+import pytest
+import numpy
+from firedrake import *
+
+import diagnostics
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+@pytest.fixture(scope='session')
+def input():
+   os.system("make -C " + cwd)
+
+def diagnostics_grid_reynolds_number():
+
+   mesh = UnitSquareMesh(10, 10)
+   fs = FunctionSpace(mesh, "CG", 1)
+   velocity = [Function(fs).interpolate(Expression("x[0]")), Function(fs).interpolate(Expression("x[1]"))]
+   density = Function(fs).interpolate(Expression("2.0"))
+   mu = Function(fs).interpolate(Expression("0.7"))
+
+   grid_re_number = diagnostics.grid_reynolds_number(mesh, density, velocity, mu)
+   grid_re_number_exact = Function(fs).interpolate(Expression("2.0*sqrt(pow(x[0], 2) + pow(x[1], 2))*0.1414213562/0.7"))
+   norm = sqrt(assemble(dot(grid_re_number - grid_re_number_exact, grid_re_number - grid_re_number_exact) * dx))
+   return norm
+
+def test_diagnostics_grid_reynolds_number(input):
+   norm = diagnostics_grid_reynolds_number()
+   print "L2 error norm:", norm
+   assert norm <= 5.0e-4
+   
+if __name__ == '__main__':
+   pytest.main(os.path.abspath(__file__))
