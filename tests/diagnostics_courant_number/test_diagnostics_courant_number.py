e47a08fba8c40d53c6c348d701a67d446411e944
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-03-20 14:19:11 +0000

    Update the tests to use the new Diagnostics class.

--- tests/diagnostics_courant_number/test_diagnostics_courant_number.py
+++ tests/diagnostics_courant_number/test_diagnostics_courant_number.py
@@ -3,7 +3,7 @@ import pytest
 import numpy
 from firedrake import *
 
-import diagnostics
+from diagnostics import *
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,7 +18,9 @@ def diagnostics_courant_number():
    velocity = [Function(fs).interpolate(Expression("x[0]")), Function(fs).interpolate(Expression("x[1]"))]
    dt = 0.5
 
-   courant_number = diagnostics.courant_number(mesh, velocity, dt)
+   diagnostics = Diagnostics(mesh, fs)
+   
+   courant_number = diagnostics.courant_number(velocity, dt)
    courant_number_exact = Function(fs).interpolate(Expression("sqrt(pow(x[0], 2) + pow(x[1], 2))*0.5/0.1414213562"))
    norm = sqrt(assemble(dot(courant_number - courant_number_exact, courant_number - courant_number_exact) * dx))
    return norm
--- tests/diagnostics_grid_reynolds_number/test_diagnostics_grid_reynolds_number.py
+++ tests/diagnostics_grid_reynolds_number/test_diagnostics_grid_reynolds_number.py
@@ -3,7 +3,7 @@ import pytest
 import numpy
 from firedrake import *
 
-import diagnostics
+from diagnostics import *
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
@@ -19,7 +19,9 @@ def diagnostics_grid_reynolds_number():
    density = Function(fs).interpolate(Expression("2.0"))
    mu = Function(fs).interpolate(Expression("0.7"))
 
-   grid_re_number = diagnostics.grid_reynolds_number(mesh, density, velocity, mu)
+   diagnostics = Diagnostics(mesh, fs)
+
+   grid_re_number = diagnostics.grid_reynolds_number(density, velocity, mu)
    grid_re_number_exact = Function(fs).interpolate(Expression("2.0*sqrt(pow(x[0], 2) + pow(x[1], 2))*0.1414213562/0.7"))
    norm = sqrt(assemble(dot(grid_re_number - grid_re_number_exact, grid_re_number - grid_re_number_exact) * dx))
    return norm
