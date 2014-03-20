fe23f887de2a7b455a7c6cc7bcec80dc546f47e0
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-03-20 17:30:24 +0000

    Removed an old variable.

--- tests/mms_les_smagorinsky/test_mms_les_smagorinsky.py
+++ tests/mms_les_smagorinsky/test_mms_les_smagorinsky.py
@@ -12,13 +12,12 @@ def mms_les_smagorinsky():
       smagorinsky_coefficient = 1.0*(1.0/n) # Remember to halve the Smagorinsky coefficient each time to compensate for the smaller filter width
       function_space = FunctionSpace(mesh, "CG", 1)
       u = [Function(function_space).interpolate(Expression('sin(x[0])')), Function(function_space).interpolate(Expression('cos(x[0])'))]
-      smagorinsky_constant = Function(function_space).interpolate(Expression("1.0"))
       density = Function(function_space).interpolate(Expression("1.0"))
       exact_solution = Function(function_space).interpolate(Expression('%f * sqrt(2.0*cos(x[0])*cos(x[0]) + 0.5*sin(x[0])*sin(x[0]) + 0.5*sin(x[0])*sin(x[0]))' % smagorinsky_coefficient))
 
       les = LES(mesh, function_space)
       visc = les.eddy_viscosity(u, density, smagorinsky_coefficient, 1.0/n)
-      print visc
+      print visc.vector()[:]
       errors.append(sqrt(assemble(dot(visc - exact_solution, visc - exact_solution) * dx)))
 
    return errors
@@ -27,7 +26,7 @@ def test_mms_les_smagorinsky():
    errors = numpy.array(mms_les_smagorinsky())
    convergence_order = numpy.log2(errors[:-1] / errors[1:])
    print "Eddy viscosity convergence order:", convergence_order
-   assert (numpy.array(convergence_order) > 0.8).all()
+   assert (numpy.array(convergence_order) > 1.6).all()
    
 if __name__ == '__main__':
    pytest.main(os.path.abspath(__file__))
