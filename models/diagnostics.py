603e45e93f6b418c53c340a38f3c91dd6b438e93
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-03-20 14:18:31 +0000

    Put all the diagnostic fields in a new class called Diagnostics.

--- models/diagnostics.py
+++ models/diagnostics.py
@@ -4,44 +4,57 @@ from firedrake import *
 
 import fields_calculations
 
-def courant_number(mesh, u, dt):
-   
-   fs = u[0].function_space()
-   w = TestFunction(fs)
-   courant_number = TrialFunction(fs)
-   solution = Function(fs)
-   
-   h = CellSize(mesh)
-   magnitude = fields_calculations.magnitude_vector(mesh, u)
-   
-   a = inner(w, courant_number)*dx
-   L = w*(magnitude*dt)/h*dx
-   solve(a == L, solution, bcs=[])
-   return solution
-   
-def grid_reynolds_number(mesh, density, u, mu):
-   
-   fs = u[0].function_space()
-   w = TestFunction(fs)
-   re = TrialFunction(fs)
-   solution = Function(fs)
-   
-   h = CellSize(mesh)
-   magnitude = fields_calculations.magnitude_vector(mesh, u)
-   
-   a = inner(w, re)*dx
-   L = (w*(density*magnitude*h)/mu)*dx
-   solve(a == L, solution, bcs=[])
-   return solution
+class Diagnostics:
 
-def divergence(vector):
+   def __init__(self, mesh, function_space):
+      self.mesh = mesh
+      self.function_space = function_space
+      
+      self.test = TestFunction(self.function_space)
+      self.trial = TrialFunction(self.function_space)
+      
+      return
 
-   w = TestFunction(vector.function_space())
-   divergence = TrialFunction(vector.function_space())
-   solution = Function(vector.function_space())
+   def courant_number(self, u, dt):
+      ''' Compute the Courant number given by 
+             (u*dt)/dx
+          where u is the velocity, dt is the time-step, and dx is the characteristic element length. '''
+      solution = Function(self.function_space)
+      
+      h = CellSize(self.mesh)
+      magnitude = fields_calculations.magnitude_vector(self.mesh, u)
+      
+      a = inner(self.test, self.trial)*dx
+      L = (self.test*(magnitude*dt)/h)*dx
+      
+      solve(a == L, solution, bcs=[])
+      return solution
    
-   a = inner(w, divergence)*dx
-   L = w*div(vector)*dx
-   solve(a == L, solution, bcs=[])
-   return solution
+   def grid_reynolds_number(self, rho, u, mu):
+      ''' Compute the grid Reynolds number given by 
+             (rho*u*dx)/mu
+          where rho is the density, u is the velocity, 
+          dx is the characteristic element length, and mu is the viscosity. '''
+          
+      solution = Function(self.function_space)
+      
+      h = CellSize(self.mesh)
+      magnitude = fields_calculations.magnitude_vector(self.mesh, u)
+      
+      a = inner(self.test, self.trial)*dx
+      L = (self.test*(rho*magnitude*h)/mu)*dx
+      
+      solve(a == L, solution, bcs=[])
+      return solution
+
+   def divergence(self, u):
+      ''' Compute the divergence of a vector field u. Returns a scalar quantity. '''
+      
+      solution = Function(self.function_space)
+      
+      a = inner(self.test, self.trial)*dx
+      L = self.test*div(u)*dx
+      
+      solve(a == L, solution, bcs=[])
+      return solution
 
