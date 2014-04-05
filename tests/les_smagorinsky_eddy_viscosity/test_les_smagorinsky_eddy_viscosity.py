aaf258370a382d15b1d654508b0dbb31dd1b42c7
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-04-05 23:49:55 +0100

    Compare against the same exact solution throughout.

--- tests/les_smagorinsky_eddy_viscosity/test_les_smagorinsky_eddy_viscosity.py
+++ tests/les_smagorinsky_eddy_viscosity/test_les_smagorinsky_eddy_viscosity.py
@@ -7,27 +7,31 @@ from les import *
 
 def les_smagorinsky_eddy_viscosity():
    errors = []
-   for n in [4, 8, 16, 32]:
+   for n in [2, 4, 8, 16, 32]:
       mesh = UnitSquareMesh(n, n)
-      smagorinsky_coefficient = 1.0
+      smagorinsky_coefficient = 2.0
       filter_width = sqrt((1.0/n)**2 + (1.0/n)**2) # FIXME: Use CellSize instead.
-      function_space = FunctionSpace(mesh, "CG", 1)
-      u = [Function(function_space).interpolate(Expression('sin(x[0])')), Function(function_space).interpolate(Expression('cos(x[0])'))]
-      density = Function(function_space).interpolate(Expression("1.0"))
-      exact_solution = Function(function_space).interpolate(Expression('pow(%f*%f, 2) * sqrt(2.0*cos(x[0])*cos(x[0]) + 0.5*sin(x[0])*sin(x[0]) + 0.5*sin(x[0])*sin(x[0]))' % (smagorinsky_coefficient, filter_width)))
+      
+      fs_exact = FunctionSpace(mesh, "CG", 3)
+      fs = FunctionSpace(mesh, "CG", 1)
+      
+      u = [Function(fs).interpolate(Expression('sin(x[0])')), Function(fs).interpolate(Expression('cos(x[0])'))]
+      density = Function(fs).interpolate(Expression("1.0"))
+      
+      exact_solution = project(Expression('pow(%f, 2) * sqrt(2.0*cos(x[0])*cos(x[0]) + 0.5*sin(x[0])*sin(x[0]) + 0.5*sin(x[0])*sin(x[0]))' % smagorinsky_coefficient), fs_exact)
 
-      les = LES(mesh, function_space)
-      visc = les.eddy_viscosity(u, density, smagorinsky_coefficient, 1.0/n)
-      print visc.vector()[:]
-      errors.append(sqrt(assemble(dot(visc - exact_solution, visc - exact_solution) * dx)))
+      les = LES(mesh, fs)
+      # Since the eddy viscosity depends on the filter_width, we need to provide smagorinsky_coefficient/filter_width here 
+      # for the convergence test because we want to compare against the same exact solution throughout.
+      eddy_viscosity = les.eddy_viscosity(u, density, (smagorinsky_coefficient/filter_width), filter_width)
+      print eddy_viscosity.vector()[:]
+      errors.append(sqrt(assemble(dot(eddy_viscosity - exact_solution, eddy_viscosity - exact_solution) * dx)))
 
    return errors
 
 def test_les_smagorinsky_eddy_viscosity():
    errors = numpy.array(les_smagorinsky_eddy_viscosity())
-   convergence_order = numpy.log2(errors[:-1] / errors[1:])
-   print "Eddy viscosity convergence order:", convergence_order
-   assert (numpy.array(convergence_order) > 1.9).all()
+   assert (numpy.array([numpy.log2(errors[i]/errors[i+1]) for i in range(len(errors)-1)]) > 1.45).all()
    
 if __name__ == '__main__':
    pytest.main(os.path.abspath(__file__))
