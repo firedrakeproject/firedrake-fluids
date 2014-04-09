147b7c2877f3de573dd7e399d0677908a5ffc6f1
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-04-09 02:03:59 +0100

    Added the pass tests for swe_dam_break_1d.

---
+++ tests/swe_dam_break_1d/test_swe_dam_break_1d.py
@@ -0,0 +1,31 @@
+import os
+import pytest
+import numpy
+from firedrake import *
+
+import shallow_water
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+@pytest.fixture(scope='session')
+def input():
+   os.system("make -C " + cwd)
+
+def swe_dam_break_1d():
+   sw = shallow_water.ShallowWater(path=os.path.join(cwd, "swe_dam_break_1d.swml"))
+   sw.run()
+   h_old = sw.solution_old.split()[-1]
+   u_old = sw.solution_old.split()[0]
+   
+   return h_old.vector().array(), u_old.vector().array()
+
+def test_swe_dam_break_1d(input):
+   h, u = swe_dam_break_1d()
+   
+   assert(max(u) <= 3.0 + 1.0e-2)   
+   assert(max(u) >= 0.0 - 1.0e-2)
+   assert(max(h) <= 5.0 + 1.0e-2)   
+   assert(max(h) >= 0.0 - 1.0e-2)
+
+if __name__ == '__main__':
+   pytest.main(os.path.abspath(__file__))
