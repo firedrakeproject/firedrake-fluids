f7ce47c269a7dcfa4a2e258779e5623fc272da7f
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-06-19 22:22:45 +0100

    Updated test tolerances.

--- tests/swe_mms_p0p1/test_swe_mms_p0p1.py
+++ tests/swe_mms_p0p1/test_swe_mms_p0p1.py
@@ -24,11 +24,11 @@ def swe_mms_p0p1():
       uy_old = sw.solution_old.split()[0][1]
       h_old = sw.solution_old.split()[1]
       
-      fs_exact = FunctionSpace(sw.mesh, "DG", 3)
+      fs_exact_dg = FunctionSpace(sw.mesh, "DG", 3)
       fs_exact_cg = FunctionSpace(sw.mesh, "CG", 3)
       
-      ux_exact = project(Expression("cos(x[0])*sin(x[1])"), fs_exact)
-      uy_exact = project(Expression("sin(x[0]*x[0]) + cos(x[1])"), fs_exact)
+      ux_exact = project(Expression("cos(x[0])*sin(x[1])"), fs_exact_dg)
+      uy_exact = project(Expression("sin(x[0]*x[0]) + cos(x[1])"), fs_exact_dg)
       h_exact = project(Expression("sin(x[0])*sin(x[1])"), fs_exact_cg)
       
       h_norms.append(sqrt(assemble(dot(h_old - h_exact, h_old - h_exact) * dx)))
@@ -42,9 +42,9 @@ def test_swe_mms_p0p1(input):
    print h_norms
    print ux_norms
    print uy_norms
-   assert (numpy.array([numpy.log2(h_norms[i]/h_norms[i+1]) for i in range(len(h_norms)-1)]) > 0.9).all()
-   assert (numpy.array([numpy.log2(ux_norms[i]/ux_norms[i+1]) for i in range(len(ux_norms)-1)]) > 0.5).all()
-   assert (numpy.array([numpy.log2(uy_norms[i]/uy_norms[i+1]) for i in range(len(uy_norms)-1)]) > 0.5).all()
+   assert (numpy.array([numpy.log2(h_norms[i]/h_norms[i+1]) for i in range(len(h_norms)-1)]) > 0.5).all()
+   assert (numpy.array([numpy.log2(ux_norms[i]/ux_norms[i+1]) for i in range(len(ux_norms)-1)]) > 0.95).all()
+   assert (numpy.array([numpy.log2(uy_norms[i]/uy_norms[i+1]) for i in range(len(uy_norms)-1)]) > 0.95).all()
    
 if __name__ == '__main__':
    pytest.main(os.path.abspath(__file__))
