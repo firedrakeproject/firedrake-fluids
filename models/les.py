be0a078feffb3955ef7a3f4a59812ef691039040
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-08-12 11:48:17 +0100

    Added license.

--- models/les.py
+++ models/les.py
@@ -1,4 +1,19 @@
-# Copyright 2013 Imperial College London. All rights reserved.
+#    Copyright (C) 2013 Imperial College London.
+
+#    This file is part of Firedrake-Fluids.
+#
+#    Firedrake-Fluids is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Firedrake-Fluids is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Firedrake-Fluids.  If not, see <http://www.gnu.org/licenses/>.
 
 from firedrake import *
 from random import random
