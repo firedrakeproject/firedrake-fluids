260a3d1685f2fa1390cf93fa3bafcf2f694e897b
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-12-21 09:18:15 +0000

    More pass tests.

--- tests/bump_tophat/test_bump_tophat.py
+++ tests/bump_tophat/test_bump_tophat.py
@@ -25,6 +25,13 @@ def bump_tophat():
 def test_bump_tophat(input):
    bump, tophat = bump_tophat()
    expected_tophat_integral = 20.0*5.0*12.0
+   expected_bump_integral = 1.45661*8.5*10*10 # The 1.45661 is the area under a bump function with x = (-1, 1) and y = (-1, 1). So we need to multiply this by 10 m * 10 m.
+   print "Expected tophat integral: %.2f" % expected_tophat_integral
+   print "Computed tophat integral: %.2f" % tophat
+   print "Expected bump integral: %.2f" % expected_bump_integral
+   print "Computed bump integral: %.2f" % bump
+
+   assert abs(bump - expected_bump_integral) <= 0.5
    assert abs(tophat - expected_tophat_integral) <= 55.0
    assert abs(bump - expected_tophat_integral) <= 55.0
    assert abs(tophat - bump) < 20.0
