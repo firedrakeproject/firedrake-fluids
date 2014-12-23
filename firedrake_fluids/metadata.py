f8a3d4b08ac483b7ae63f56985541d4745bd5dff
Christian Jacobs <c.jacobs10@imperial.ac.uk>
2014-12-23 17:21:41 +0000

    Added a new module containing metadata-related functions.

---
+++ firedrake_fluids/metadata.py
@@ -0,0 +1,12 @@
+import subprocess
+from firedrake_fluids import LOG
+
+def get_git_revision(cwd=None):
+   # From the pybench code by Florian Rathgeber.
+   # https://github.com/firedrakeproject/pybench/blob/master/pybench.py
+   try:
+      revision = subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=cwd).strip()
+   except OSError as e:
+      LOG.exception(e)
+      revision = None
+   return revision
